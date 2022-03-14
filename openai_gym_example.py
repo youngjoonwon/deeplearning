5549d941d671896f826568d2c966dbf7abf9abea
Youngjoon Won <young.joon.won@gmail.com>
2022-03-14 18:57:58 +0900

    new

---
+++ openai_gym_example.py
@@ -0,0 +1,24 @@
+import gym
+env = gym.make('CartPole-v1')
+#env = gym.make('MountainCar-v0')
+#env = gym.make('MsPacman-v0')
+
+env.reset()
+for _ in range(1000):
+    env.render()
+    env.step(env.action_space.sample()) # take a random action
+env.close()
+
+'''
+for i_episode in range(20):
+    observation = env.reset()
+    for t in range(100):
+        env.render()
+        print(observation)
+        action = env.action_space.sample()
+        observation, reward, done, info = env.step(action)
+        if done:
+            print("Episode finished after {} timesteps".format(t+1))
+            break
+env.close()
+'''
